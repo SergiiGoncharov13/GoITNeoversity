70a50259bbe5626e98058f2fdaac930a57fe854e
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2023-12-15 23:14:34 +0200

    add hw3

---
+++ python_core/goitneo-python-hw-3-DS2/classes_for_bot.py
@@ -0,0 +1,125 @@
+from collections import UserDict
+from datetime import datetime
+import pickle
+
+
+class Field:
+    def __init__(self, value):
+        self._value = None
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+    
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    @Field.value.setter
+    def value(self, value):
+        if len(value) < 10 or len(value) > 12:
+            raise ValueError("Phone must contains 10 symbols")
+        elif not value.isnumeric():
+            raise ValueError('Wrong phones.')
+        self._value = value
+
+
+class Birthday(Field):
+    @Field.value.setter
+    def value(self, value):
+        today = datetime.now().date()
+        birth_date = datetime.strptime(value, "%d-%m-%Y").date() # "%d-%m-%Y"   '%Y-%m-%d'
+        if birth_date > today:
+            raise ValueError("Wrong input")
+        self._value = value
+ 
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+        return f"The phone was added"
+   
+    def remove_phone(self,phone):
+        for r_phone in self.phones:
+            if r_phone.value == phone:
+                self.phones.remove(r_phone)
+                return f"The phone was deleted"
+            else:
+                return f"The phone {phone} isn't in your contact book"
+    
+    def edit_phone(self, phone, new_phone):
+        for val in self.phones:
+            if val.value == phone:
+                val.value = new_phone
+                return f"This phone was changed"
+            else:
+                return f"This phone {phone} isn't in your contact book"
+
+    def find_phone(self, phone):
+        for phone in self.phones:
+            if phone in self.phones:
+                return f"{phone}"
+            else:
+                return f"This phone {phone} isn't in your contact book"
+
+    def add_birthday(self, date):
+        self.birthday = Birthday(date)
+        return f"The birthday was set successfully"
+
+
+class AddressBook(UserDict):
+    def __init__(self):
+        super().__init__()
+        self.load_from_file()
+
+    def add_record(self, record):
+        self.data[record.name.value] = record
+        return f"New contact was added"
+
+    def delete_record(self, name):
+        del self.data[name]
+        return f"{self.data[name]} was removed"
+
+    def get_record(self, name) -> Record:
+        return self.data.get(name)
+    
+    def search(self, value):
+        if self.has_record(value):
+            return self.get_record(value)
+        for record in self.get_all_record().values():
+            for phone in record.phones:
+                if phone.value == value:
+                    return record
+        raise ValueError('Contact not found')
+
+    def save_to_file(self):
+        with open('contacts_book.bin', 'wb') as file:
+            pickle.dump(self.data, file)
+        return f"contacts book saved"
+
+    def load_from_file(self):
+        try:
+            with open('contacts_book.bin', 'rb') as file:
+                self.data = pickle.load(file)
+            return f"loaded from contact book"
+        except FileNotFoundError:
+            pass
+
---
+++ python_core/goitneo-python-hw-3-DS2/console_bot3.py
@@ -0,0 +1,130 @@
+from datetime import date
+from classes_for_bot import AddressBook, Record
+
+USERS = AddressBook()
+
+
+def error_handler(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            return "This contact doesnt exist, please try again"
+        except ValueError as exception:
+            return exception.args[0]
+        except IndexError:
+            return "This contact cannot be added, it exists already"
+        except TypeError:
+            return "Unknown command, please try again"
+        except AttributeError:
+            return "Opps"
+    return inner
+
+
+@error_handler
+def add_contact(args):
+    name, phone = args
+    if name in USERS:
+        raise ValueError("This user already exist")
+    record = Record(name)
+    record.add_phone(phone)
+    USERS.add_record(record)
+    return f"User {name}: {phone} added"
+
+@error_handler
+def change_contact(args):
+    name, phone = args
+    USERS[name] = phone
+    return f"User {name} have a new phone number"
+
+@error_handler
+def show_phone(args):
+    user = args[0]
+    phone = USERS[user]
+    return f"{user}: {phone}"
+   
+@error_handler
+def show_all(_):
+    result = ""
+    for name, phone in USERS.items():
+        result += f"{name}: {phone} \n"
+    return result
+
+@error_handler
+def add_birthday(data):
+    name, data = data.strip().split(" ")
+    record = USERS[name]
+    record.add_birthday(data)
+    return f"For {name} you add birthday {data}"
+
+@error_handler
+def show_birthday(args):
+    user = args[0]
+    birthday = USERS[user]
+    return f"{user}: {birthday}"
+
+@error_handler
+def get_birthdays_per_week():
+    pass
+
+@error_handler
+def save_address_book(*_):
+    return USERS.save_to_file()
+
+@error_handler
+def load_address_book(*_):
+    return USERS.load_from_file()
+    
+def hello(_):
+    return "How can I help you?"
+    
+def exit(_):
+    return "Good bye!"
+
+
+HANDLERS = {
+    "hello": hello,
+    "close": exit,
+    "exit": exit,
+    "add": add_contact,
+    "change": change_contact,
+    "all": show_all,
+    "phone": show_phone,
+    "add-birthday": add_birthday,
+    "show-birthday": show_birthday,
+    "birthdays": get_birthdays_per_week, 
+    "save": save_address_book,
+    "load": load_address_book
+
+}
+
+
+@error_handler
+def parser_input(user_input):
+    cmd, *args = user_input.strip().split(' ')
+    try:
+        handler = HANDLERS[cmd.lower()]
+    except KeyError:
+        if args:
+            cmd = f"{cmd} {args[0]}"
+            args = args[1:]
+        handler = HANDLERS[cmd.lower(), "Unknown command"]
+    return handler, args
+
+
+def main():
+    while True:
+        user_input = input("Enter command> ")
+        if user_input in ("close", "exit"):
+            print("Good bye!")
+            break
+        handler, *args = parser_input(user_input)
+        result = handler(*args)
+        if not result:
+            print("Good bye!")
+            break
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
