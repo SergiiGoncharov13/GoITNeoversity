6078d2ff7a2d6d48c432da0f03d7845d5ad6f523
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2024-02-24 22:32:59 +0200

    hw4

---
+++ basic_algorithms/merge.py
@@ -0,0 +1,42 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    return merge(merge_sort(left_half), merge_sort(right_half))
+
+
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    # Merge smaller elements first
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+
+    # If there are any items left in the left or right half,
+    # add them to the result
+    while left_index < len(left):
+        merged.append(left[left_index])
+        left_index += 1
+
+    while right_index < len(right):
+        merged.append(right[right_index])
+        right_index += 1
+
+    return merged
+
+
+if __name__ == '__main__':
+    arr = [5, 3, 8, 4, 2]
+    sorted_arr = merge_sort(arr)
+    print(arr, sorted_arr)
